2096d16f933e5ecdd29bc642a2fef637f26e626e
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-05-28 15:20:07 +0200

    navbar gefixt

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,9 +2,7 @@
     <div class="container mx-auto px-6 py-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold">KiteSurfschool Windkracht-12</h1>
         <nav class="space-x-4">
-            <x-nav-link href="#home" :active="request()->is('/')">Home</x-nav-link>
-            <x-nav-link href="#about" :active="request()->is('about')">About</x-nav-link>
-            <x-nav-link href="#contact" :active="request()->is('contact')">Contact</x-nav-link>
+            <x-nav-link href="{{ url('/') }}" :active="request()->is('/')">Home</x-nav-link>
             <x-nav-link href="{{ route('book.lesson') }}" :active="request()->is('book-lesson')">Book a Lesson</x-nav-link>
             <x-nav-link href="{{ route('login') }}" :active="request()->is('login')">
                 <span class="px-4 py-2 border border-gray-300 rounded hover:bg-gray-100">Log in</span>
